575d118ec1ae4a6a6181dba60ac43be577bd514c
Essonti <Strofim4enko@yandex.ru>
2021-08-15 00:26:37 +0300

    feat: add code for 3 functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
 exports.min = function min (array) {
-  return 0;
+  let result = array[0];
+  if (array === ['']) {
+  result = array.reduce((a,b) => a < b ? a : b);
+  } else result = 0;
+  return result;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let result = array[0];
+  if (array !== undefined) {
+  result = array.reduce((a,b) => a > b ? a : b);
+  } else result = 0;
+  return result;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let sum = 0;
+for (let i = 0; i < array.length; i++) {
+sum += array[i];
+}
+let result = sum / array.length;
+return result;
 }
